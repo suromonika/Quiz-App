18371cf9b4f2b6d24044cd2761ed505fc4273301
suromonika <suro.monika@gmail.com>
2022-09-26 19:12:04 +0200

    Second version with winner and loser messages

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ let correctAnswers = 0;
 let incorrectAnswers = 0;
 const scoreContainer = document.querySelector('#score-container');
 const mistakesContainer = document.querySelector('#mistakes-container');
+const appInnerWrapper = document.querySelector('.app-inner-wrapper');
+const wrongElement = document.querySelector('.correct-wrong');
+let currentQuestionsIndex = 0;
+let shuffledQuestions = 0;
 
 const questions = [
   {
@@ -21,35 +25,99 @@ const questions = [
     ],
   },
   {
-    question: 'What is 5 + 9?',
+    question: 'What does Prison Mike say is the worst thing about jail?',
     answers: [
-      { option: 14, correct: true },
-      { option: 5, correct: false },
-      { option: 69, correct: false },
-      { option: 420, correct: false },
+      { option: 'The food', correct: false },
+      { option: 'The showers', correct: false },
+      { option: 'The Dementors', correct: true },
+      { option: 'Sleepless nights', correct: false },
     ],
   },
   {
-    question: 'What is 2 * 12?',
+    question:
+      "What does Michael offer Scott's tots when he can't pay their tuition?",
     answers: [
-      { option: 24, correct: true },
-      { option: 5, correct: false },
-      { option: 69, correct: false },
-      { option: 420, correct: false },
+      { option: 'Pizza Hut discount coupons', correct: false },
+      { option: 'Boxes of crayons', correct: false },
+      { option: 'Calculators', correct: false },
+      { option: 'Lithium batteries', correct: true },
+    ],
+  },
+  {
+    question: "What is Stanley's favorite day?",
+    answers: [
+      { option: 'Pay Day', correct: false },
+      { option: 'Pretzel Day', correct: true },
+      { option: 'Pancake Day', correct: false },
+      { option: 'Casino Day', correct: false },
+    ],
+  },
+  {
+    question:
+      "When Michael visits ex-girlfriend Holly's office in Nashua, what does he take as a souvenir?",
+    answers: [
+      { option: 'Sleeve of her sweater', correct: true },
+      { option: "Holly's Woody Doll", correct: false },
+      { option: 'An envelope with a letter', correct: false },
+      { option: "Her fiance's picture", correct: false },
+    ],
+  },
+  {
+    question: "Which Angela's cat did Dwight put into a freezer?",
+    answers: [
+      { option: 'Sprinkles', correct: true },
+      { option: 'Princess Lady', correct: false },
+      { option: 'Bandit', correct: false },
+      { option: 'Lumpy', correct: false },
+    ],
+  },
+  {
+    question: "What is the title of Michael's movie?",
+    answers: [
+      { option: 'Nightly Threat Mission', correct: false },
+      { option: 'Midnight Mission', correct: false },
+      { option: 'Night Agent Scarn', correct: false },
+      { option: 'Threat Level Midnight', correct: true },
+    ],
+  },
+  {
+    question: "What is the name of Pam, Oscar and Toby's club?",
+    answers: [
+      { option: 'The Breakfast Club', correct: false },
+      { option: 'The Fine Literature Club', correct: false },
+      { option: 'The Afternoon Tea Club', correct: false },
+      { option: 'The Finer Things Club', correct: true },
+    ],
+  },
+  {
+    question: "What is Dwight's all-time favorite movie?",
+    answers: [
+      { option: 'The Crow', correct: true },
+      { option: 'Battlestar Galactica', correct: false },
+      { option: 'Star Wars', correct: false },
+      { option: 'Robocop', correct: false },
+    ],
+  },
+  {
+    question: 'Why was Dwight removed as safety officer?',
+    answers: [
+      { option: 'He gave Stanley a heart attack', correct: false },
+      { option: 'He set a trashcan on fire in the office', correct: false },
+      { option: "He didn't let anyone out of the office", correct: false },
+      { option: 'He shot a gun at the office', correct: true },
     ],
   },
 ];
 
 function startGame() {
   questionContainer.classList.remove('hide');
-  nextBtn.classList.remove('hide');
   startBtn.classList.add('hide');
   randomizeQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuestionsIndex = 0;
   nextQuestion();
 }
 
 function nextQuestion() {
+  resetState();
   showQuestion(randomizeQuestions[currentQuestionsIndex]);
 }
 
@@ -58,6 +126,7 @@ function showQuestion(question) {
   question.answers.forEach((answer) => {
     answerButton = document.createElement('button');
     answerButton.textContent = answer.option;
+    answerButton.id = 'answer-btn';
     answerButton.classList.add('btn');
     answerBtns.appendChild(answerButton);
     if (answer.correct) {
@@ -67,32 +136,69 @@ function showQuestion(question) {
   });
 }
 
-const wrongElement = document.querySelector('.correct-wrong');
-
 function selectedAnswer(e) {
   const selectedAnswer = e.target;
   const correct = selectedAnswer.dataset.correct;
-  const incorrect = selectedAnswer.dataset.correct;
   if (correct) {
     correctAnswers++;
   } else if (!correct) {
     incorrectAnswers++;
   }
-  showCorrectOrWrong(wrongElement, correct, incorrect);
+  showCorrectOrWrong(wrongElement, correct, !correct);
+  displayScore();
+  displayMistakes();
+
+  //Shows next questions; if no more questions available, shows winner window and restart button
+  if (randomizeQuestions.length > currentQuestionsIndex + 1) {
+    nextBtn.classList.remove('hide');
+  } else {
+    const winnerImage = document.createElement('img');
+    winnerImage.src = '/img/winner.gif';
+    document.querySelector('#loser-winner').appendChild(winnerImage);
+    appInnerWrapper.classList.add('hide');
+    restartGame();
+  }
+}
+
+function restartGame() {
+  const restartButton = document.createElement('button');
+  restartButton.textContent = 'Restart';
+  restartButton.classList.add('btn');
+  document.querySelector('#loser-winner').appendChild(restartButton);
+  restartButton.addEventListener('click', () => window.location.reload());
+}
+
+function displayScore() {
   const CorrectCountID = 'score-output';
-  if (document.querySelector('#score-ouput')) {
+  if (document.querySelector('#score-output')) {
     document.querySelector('#score-output').remove();
   }
   const correctCount = document.createElement('div');
   correctCount.id = 'score-output';
-  correctCount.textContent = correctAnswers.toString();
+  correctCount.textContent = `Score ${correctAnswers.toString()}`;
   correctCount.classList.add('score');
   scoreContainer.appendChild(correctCount);
+}
 
-  incorrectCount = document.createElement('div');
-  incorrectCount.textContent = incorrectAnswers.toString();
-  incorrectCount.classList.add('mistake');
-  mistakesContainer.appendChild(incorrectCount);
+function displayMistakes() {
+  const MistakesCountID = 'mistakes-output';
+  if (document.querySelector('#mistakes-output')) {
+    document.querySelector('#mistakes-output').remove();
+  }
+  const mistakesCount = document.createElement('div');
+  mistakesCount.id = 'mistakes-output';
+  mistakesCount.textContent = `${incorrectAnswers.toString()}/3`;
+  mistakesCount.classList.add('mistake');
+  mistakesContainer.appendChild(mistakesCount);
+  //When certain amount of mistakes reached - shows loser window with restart button
+  if (incorrectAnswers == 3) {
+    nextBtn.classList.add('hide');
+    appInnerWrapper.classList.add('hide');
+    const loserImage = document.createElement('img');
+    loserImage.src = '/img/loser.gif';
+    document.querySelector('#loser-winner').appendChild(loserImage);
+    restartGame();
+  }
 }
 
 function showCorrectOrWrong(element, correct, incorrect) {
@@ -102,5 +208,16 @@ function showCorrectOrWrong(element, correct, incorrect) {
     element.classList.add('wrong');
   }
 }
+function resetState() {
+  wrongElement.classList.remove('correct');
+  wrongElement.classList.remove('wrong');
+  nextBtn.classList.add('hide');
+  while (answerBtns.firstChild) {
+    answerBtns.removeChild(answerBtns.firstChild);
+  }
+}
 
-// nextBtn.addEventListener('click');
+nextBtn.addEventListener('click', () => {
+  currentQuestionsIndex++;
+  nextQuestion();
+});
